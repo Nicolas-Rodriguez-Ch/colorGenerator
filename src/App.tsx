231fcad2672914f25409ba77127fb13429d79074
Nicolás Rodrígues Chaves <nicolasrodriguezch@hotmail.com>
2025-02-03 10:31:57 -0500

    identation

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ function App() {
   const { color, generateColor } = useGenerateRandomColor();
   return (
     <>
-      <div 
-        className={`h-full w-full bg-blue-900`}
+      <div
+        className={`h-full w-full bg-blue-900 flex justify-center items-center`}
       >
-          aqui va el bg
+        aqui va el bg
       </div>
     </>
   );

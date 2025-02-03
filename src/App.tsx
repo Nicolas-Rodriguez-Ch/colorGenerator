b99c33561713f336a3c355d227bfc682e8336474
Nicolás Rodrígues Chaves <nicolasrodriguezch@hotmail.com>
2025-02-03 11:07:55 -0500

    starts tracking generated colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
+import { useState } from 'react';
 import './App.css';
 import useGenerateRandomColor from './hooks/useGenerateRandomColor';
 
 function App() {
   const { color, generateColor } = useGenerateRandomColor();
-  console.log('🚀 ~ App ~ color:', color);
-  
+  const [generatedColors, setGeneratedColors] = useState<string[]>([]);
+  console.log('🚀 ~ App ~ generatedColors:', generatedColors);
+  const generateBaseColor = () => {
+    generateColor();
+    setGeneratedColors((prev) => [...prev, color]);
+  };
+
   return (
     <>
       <div
-        className={`h-dvh w-full bg-blue-900 flex justify-center items-center`}
+        className={`h-dvh w-full flex justify-center items-center`}
+        style={{ backgroundColor: '#' + color }}
       >
         <button
           className='p-10 rounded-xl text-2xl bg-teal-800 cursor-pointer'
-          onClick={generateColor}
+          onClick={generateBaseColor}
+          style={{
+            backgroundImage:
+              'linear-gradient(to top, #a8edea 0%, #fed6e3 100%)',
+          }}
         >
           Generate Random Color!
         </button>
+        <>
+          {generatedColors !== null ? (
+            <div>Your generated colors</div>
+          ) : (
+            <div>You haven't generated any colors yet</div>
+          )}
+        </>
       </div>
     </>
   );
--- src/hooks/useGenerateRandomColor.ts
+++ src/hooks/useGenerateRandomColor.ts
@@ -4,7 +4,11 @@ const useGenerateRandomColor = () => {
   const [color, setColor] = useState('');
 
   const generateColor = () => {
-    setColor(Math.random().toString(16).substring(-6));
+    setColor(
+      Math.floor(Math.random() * 0xffffff)
+        .toString(16)
+        .padStart(6, '0')
+    );
   };
 
   return { color, generateColor };
